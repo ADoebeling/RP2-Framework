99081efca74b2a94be5c8b30a1d1c676d3362409
Andreas Döbeling <ad@1601.com>
2015-10-29 16:49:40 +0100

    Update mgntRatioExportDomainPrices.php

--- config/mgntRatioExportDomainPrices.php
+++ config/mgntRatioExportDomainPrices.php
@@ -3,10 +3,10 @@
 $resellerDiscount = 0; // %
 $domainDeDiscount = 0; // %
 
-$c['domain']['DOM_DE']['externalCosts'] =          0.79/1.19;
+$c['domain']['DOM_DE']['externalCosts'] =          0.79*$domainDeDiscount/1.19;
 $c['domain']['DOM_AT']['externalCosts'] =          1.49/1.19;
-$c['domain']['DOM_BIZ']['externalCosts'] =         1.39/1.19;
-$c['domain']['DOM_CC']['externalCosts'] =          3.99/1.19;
+$c['domain']['DOM_BIZ']['externalCosts'] =         1.39*$resellerDiscount/1.19;
+$c['domain']['DOM_CC']['externalCosts'] =          3.99*$resellerDiscount/1.19;
 $c['domain']['COM_CO']['externalCosts'] =          1.39/1.19;
 $c['domain']['DOM_COM']['externalCosts'] =         1.39/1.19;
 $c['domain']['DOM_EU']['externalCosts'] =          0.99/1.19;
@@ -18,4 +18,6 @@ $c['domain']['DOM_NET']['externalCosts'] =         1.39/1.19;
 $c['domain']['DOM_ORG']['externalCosts'] =         1.39/1.19;
 $c['domain']['DOM_TV']['externalCosts'] =          4.99/1.19;
 $c['domain']['DOM_WS']['externalCosts'] =          2.49/1.19;
+
+$c['domain']['DOM_CHEAP']['externalCosts'] =       3.99*$resellerDiscount/1.19;
 
